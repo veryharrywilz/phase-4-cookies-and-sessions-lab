af5730255fb4dc2be033dfb3dda5662523a864b0
veryharrywilz <110489443+veryharrywilz@users.noreply.github.com>
2022-12-01 16:27:43 -0500

    commit tree cde9cfd3af745725c237a82a953a40d62f694439

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,8 +7,16 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    article = Article.find(params[:id])
-    render json: article
+
+    session[:page_views] ||= 0
+    session[:page_views] += 1
+
+    if session[:page_views] > 3
+      render json: { error: "Maximum pageview limit reached" }, status: 401
+    else
+      article = Article.find(params[:id])
+      render json: article
+    end
   end
 
   private
